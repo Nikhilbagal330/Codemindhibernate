3fc9ef161aa91b82daeb48c32eb18dade1379cab
nikhi <nikhi@NikhilBagal>
2023-12-07 17:08:16 +0530

    second commit

--- Codemindhibernate/src/main/java/hibernate/MainHibernateApplication.java
+++ Codemindhibernate/src/main/java/hibernate/MainHibernateApplication.java
@@ -1,5 +1,6 @@
 package hibernate;
 
+import java.util.*;
 import org.hibernate.Session;
 
 public class MainHibernateApplication {
@@ -11,25 +12,30 @@ public class MainHibernateApplication {
 		
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
-		
-		
-		
+		Scanner sc=new Scanner(System.in);
+	
+		String id,name,city,course;
+		id=sc.nextLine();
+		name=sc.nextLine();
+		city=sc.nextLine();
+		course=sc.nextLine();
 		
 		Student st1 = new Student();
-		st1.setId("56474");
-		st1.setName("Mohit");
-		st1.setCity("Bhopal");
-		st1.setCourse("maths");
+		st1.setId(id);
+		st1.setName(name);
+		st1.setCity(city);
+		st1.setCourse(course);
 		st1.setSchool("VVPS");
 		session.save(st1);
 		
-		Student st2 = new Student();
-		st1.setId("56474");
-		st1.setName("Mohit");
-		st1.setCity("Bhopal");
-		st1.setCourse("maths");
-		st1.setSchool("VVPS");
-		session.save(st2);
+		
+//		Student st2 = new Student();
+//		st1.setId("56474");
+//		st1.setName("nikhil");
+//		st1.setCity("Bhopal");
+//		st1.setCourse("maths");
+//		st1.setSchool("VVPS");
+//		session.save(st2);
 
 		
 		//Student st  = session.load(Student.class, "457");
